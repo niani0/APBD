cf06dffdf2100c8b4f15b676954e00b922f18db7
Franciszek Osiak <s22852@pjwstk.edu.pl>
2022-06-05 19:05:27 +0200

    commit

---
+++ MvcMovie/Startup.cs
@@ -0,0 +1,58 @@
+using Microsoft.EntityFrameworkCore;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration, IWebHostEnvironment env)
+    {
+        Environment = env;
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+    public IWebHostEnvironment Environment { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllersWithViews();
+
+        services.AddDbContext<MvcMovieContext>(options =>
+        {
+            var connectionString = Configuration.GetConnectionString("MvcMovieContext");
+
+            if (Environment.IsDevelopment())
+            {
+                options.UseSqlite(connectionString);
+            }
+            else
+            {
+                options.UseSqlServer(connectionString);
+            }
+        });
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+        });
+    }
+}
